7efc05ffcb085e92cd002648e4d439d7398bf421
anarkrypto <anarkrypto@gmail.com>
2022-12-12 00:50:08 -0300

    👔 Ensure no repeated nickname, address or price - For each month

--- pages/api/guesses.ts
+++ pages/api/guesses.ts
@@ -7,6 +7,7 @@ import { convert, Unit } from 'nanocurrency';
 import { TunedBigNumber } from "utils/nano";
 import { IPaymentResponse } from "types.ts/checkout";
 import { GuessData } from "types.ts/guess";
+import { Op } from "sequelize";
 
 const OPEN_DAY = Number(process.env.NEXT_PUBLIC_OPEN_DAY || DEFAULT_OPEN_DAY);
 const CLOSE_DAY = Number(process.env.NEXT_PUBLIC_CLOSE_DAY || DEFAULT_CLOSE_DAY);
@@ -18,90 +19,143 @@ const guesses = new Guesses();
 guesses.sync();
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-    
-    if (req.method === 'GET') {
 
-        const values = await guesses.readAll();
-        return res.status(200).json(values);
+    try {
+        if (req.method === 'GET') {
 
-    } else if (req.method === 'POST') {
+            const values = await guesses.readAll();
+            return res.status(200).json(values);
 
-        const today = () => new Date().getDate();
+        } else if (req.method === 'POST') {
 
-        // Competition of the month not yet started
-        if (today() < OPEN_DAY) return res.status(400).json({ error: "not started yet" })
+            const today = () => new Date().getDate();
 
-        // Competition of the month finished
-        if (today() >= CLOSE_DAY) {
-            return res.status(400).json({ error: "finished" })
-        }
+            // Competition of the month not yet started
+            if (today() < OPEN_DAY) return res.status(400).json({ error: "not started yet" })
 
-        // Parse user json request 
-        let json: GuessData | any = req.body;
-        if (typeof json !== "object") {
-            try {
-                json = JSON.parse(req.body)
-            } catch (e) {
+            // Competition of the month finished
+            if (today() >= CLOSE_DAY) {
+                return res.status(400).json({ error: "finished" })
+            }
+
+            // Parse user json request 
+            let json: GuessData | any = req.body;
+            if (typeof json !== "object") {
+                try {
+                    json = JSON.parse(req.body)
+                } catch (e) {
+                    return res.status(400).json({
+                        error: "unable to parse JSON"
+                    });
+                }
+            }
+
+            const response = await axios.get(
+                `https://api.nanobytepay.com/payments/${json.paymentId}`,
+                {
+                    headers: {
+                        "x-api-key": CHECKOUT_API_KEY,
+                    },
+                }
+            );
+
+            const payment: IPaymentResponse = response.data;
+
+            if (payment.status !== 'confirmed') {
                 return res.status(400).json({
-                    error: "unable to parse JSON"
+                    error: 'payment not confirmed'
                 });
             }
-        }
 
-        const response = await axios.get(
-            `https://api.nanobytepay.com/payments/${json.paymentId}`,
-            {
-                headers: {
-                    "x-api-key": CHECKOUT_API_KEY,
-                },
-            }
-        );
-
-        const payment: IPaymentResponse = response.data;
-
-        if (payment.status !== 'confirmed') {
-            return res.status(400).json({
-                error: 'payment not confirmed'
-            });
-        }
-
-        // Validate user guess
-        try {
-            validateGuess(payment.metadata);
-        } catch (err) {
-            return res.status(400).json({
-                error: err
-            });
-        }
-
-        // Get guess data from checkout metadata
-        const { userNickname, userNanoAddress, userGuessPrice, amount } = payment.metadata;
-
-        // Ensure user has paid enough
-        if (TunedBigNumber(amount).isLessThan(PRICE_GUESS_NANO)) {
-            return res.status(402).json({ error: 'insufficient amount' });
-        }
-
-        // Save user guess to database
-        guesses.create({
-            nickname: userNickname,
-            address: userNanoAddress,
-            price: Number(userGuessPrice),
-            hash: json.paymentId
-        })
-            .then(resp => {
-                res.status(200).json({
-                    successful: true,
-                    ...resp
+            // Validate user guess
+            try {
+                validateGuess(payment.metadata);
+            } catch (err) {
+                return res.status(400).json({
+                    error: err
                 });
-            })
-            .catch(err => {
-                res.status(400).json(err)
-            })
+            }
 
-    } else {
-        return res.status(405).send({
-            message: 'Only POST requests allowed'
+            // Get guess data from checkout metadata
+            const { userNickname: nickname, userNanoAddress: address, userGuessPrice: price, amount } = payment.metadata;
+
+            // Ensure user has paid enough
+            if (TunedBigNumber(amount).isLessThan(PRICE_GUESS_NANO)) {
+                return res.status(402).json({ error: 'insufficient amount' });
+            }
+
+            const startDate = new Date(new Date().setUTCDate(OPEN_DAY)).setUTCHours(0, 0, 0, 0);
+            const endDate = new Date(new Date().setUTCDate(CLOSE_DAY)).setUTCHours(23, 59, 59, 999);
+
+            // Ensure nikcname is not registered for this month competition
+            const nicknameExists = await guesses.find({
+                nickname,
+                createdAt: {
+                    [Op.gt]: startDate,
+                    [Op.lt]: endDate
+                }
+            });
+            if (nicknameExists) {
+                return res.status(400).json({
+                    error: 'nickname already exists'
+                });
+            }
+
+            await guesses.sync();
+
+            // Ensure address is not registered for this month competition
+            const addressExists = await guesses.find({
+                address,
+                createdAt: {
+                    [Op.gt]: startDate,
+                    [Op.lt]: endDate
+                }
+            });
+            if (addressExists) {
+                return res.status(400).json({
+                    error: 'address already exists'
+                });
+            }
+
+            // Ensure price is not registered for this month competition
+            const priceExists = await guesses.find({
+                price,
+                createdAt: {
+                    [Op.gt]: startDate,
+                    [Op.lt]: endDate
+                }
+            });
+            if (priceExists) {
+                return res.status(400).json({
+                    error: 'price already exists'
+                });
+            }
+
+            // Save user guess to database
+            guesses.create({
+                nickname,
+                address,
+                price: Number(price),
+                hash: json.paymentId
+            })
+                .then(resp => {
+                    res.status(200).json({
+                        successful: true,
+                        ...resp
+                    });
+                })
+                .catch(err => {
+                    res.status(400).json(err)
+                })
+
+        } else {
+            return res.status(405).send({
+                message: 'Only POST requests allowed'
+            });
+        }
+    } catch (err) {
+        return res.status(500).json({
+            error: err
         });
     }
 }
