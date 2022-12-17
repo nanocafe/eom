ecb0c778d2a0170ecff7f49ef5471597d9e15a1a
anarkrypto <anarkrypto@gmail.com>
2022-12-17 10:38:45 -0300

    ⚡️ Add cache to coinmarketcap  price api

--- pages/api/price.ts
+++ pages/api/price.ts
@@ -1,23 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import cache from "memory-cache";
 import axios from "axios";
 
 const XNO_ID = 1567;
 const CONVERT = "USD";
+const CACHE_TIME = 5 * 60 * 1000; // 5 minutes in milliseconds
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     try {
-        const response = await axios.get(
-            `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=${XNO_ID}&convert=${CONVERT}`,
-            {
-                headers: {
-                    "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY,
-                    "Accept": "application/json",
-                },
-            }
-        );
-        res.status(200).json({
-            ...response.data.data[XNO_ID].quote[CONVERT],
-        });
+        const url = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=${XNO_ID}&convert=${CONVERT}`;
+        const cachedResponse = cache.get(url);
+        if (cachedResponse) {
+            res.status(200).json({
+                ...cachedResponse
+            });
+        } else {
+            const response = await axios.get(
+                url,
+                {
+                    headers: {
+                        "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY,
+                        "Accept": "application/json",
+                    },
+                }
+            );
+            const data = response.data.data[XNO_ID].quote[CONVERT];
+            cache.put(url, data, CACHE_TIME);
+            res.status(200).json({
+                ...data,
+            });
+        }
     } catch (e) {
         console.log("error", e)
         res.status(500).json({
