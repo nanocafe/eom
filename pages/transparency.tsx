4659df23eb9cc7c91c3a44db4b407a69e9d6eac1
anarkrypto <anarkrypto@gmail.com>
2022-12-17 11:19:14 -0300

    ✨ Add empty Transparency page

---
+++ pages/transparency.tsx
@@ -0,0 +1,12 @@
+import Layout from "components/Layout";
+
+export default function Transparency () {
+    return (
+        <Layout navbarOption={{
+            name: 'Home',
+            href: '/'
+        }}>
+            <div></div>
+        </Layout>
+    )
+}
