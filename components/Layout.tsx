958ba55c16ff061794c40be291b5923109bbc5d6
anarkrypto <anarkrypto@gmail.com>
2023-03-05 23:59:36 -0300

    🚸 Update meta title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children, showNavbar = true, navbarOption }: La
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>NanoCafe EOM</title>
+        <title>EOM by NanoCafe</title>
       </Head>
 
       <main className='w-full flex flex-col items-center'>
